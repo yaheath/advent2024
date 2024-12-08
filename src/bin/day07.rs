e80f152dde7a768b129ee3ef6d598cdbe014c471
Heath Kehoe <yaheath@gmail.com>
2024-12-08 10:50:43 -0600

    day 7 & 8

---
+++ src/bin/day07.rs
@@ -0,0 +1,95 @@
+use itertools::{Itertools, repeat_n};
+use std::str::FromStr;
+use std::vec::Vec;
+use ya_advent_lib::read::read_input;
+
+struct Input {
+    lhs: u64,
+    rhs: Vec<u64>,
+}
+
+impl FromStr for Input {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (lhs_s, rhs_s) = s.split_once(": ").unwrap();
+        let lhs = lhs_s.parse::<u64>().unwrap();
+        let rhs = rhs_s.split(' ').flat_map(|ss| ss.parse::<u64>()).collect();
+        Ok(Input{lhs, rhs})
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Oper {
+    Add,
+    Mul,
+    Cat,
+}
+
+impl Input {
+    fn is_valid(&self, forpart2: bool) -> bool {
+        let ops = if forpart2 {
+            vec![Oper::Add, Oper::Mul, Oper::Cat]
+        }
+        else {
+            vec![Oper::Add, Oper::Mul]
+        };
+        for ops in repeat_n(ops, self.rhs.len() - 1)
+                .multi_cartesian_product() {
+            let mut ops_iter = ops.iter();
+            let e = self.rhs
+                .iter()
+                .copied()
+                .reduce(|acc, n| {
+                    match ops_iter.next() {
+                        Some(Oper::Add) => acc + n,
+                        Some(Oper::Mul) => acc * n,
+                        Some(Oper::Cat) => format!("{acc}{n}").parse::<u64>().unwrap(),
+                        _ => panic!(),
+                    }
+                })
+                .unwrap();
+            if e == self.lhs {
+                return true;
+            }
+        }
+        false
+    }
+
+}
+
+fn part1(input: &[Input]) -> u64 {
+    input.iter().filter(|i| i.is_valid(false)).map(|i| i.lhs).sum()
+}
+
+fn part2(input: &[Input]) -> u64 {
+    input.iter().filter(|i| i.is_valid(true)).map(|i| i.lhs).sum()
+}
+
+fn main() {
+    let input: Vec<Input> = read_input();
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ya_advent_lib::read::test_input;
+
+    #[test]
+    fn day07_test() {
+        let input: Vec<Input> = test_input(
+"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+");
+        assert_eq!(part1(&input), 3749);
+        assert_eq!(part2(&input), 11387);
+    }
+}
---
+++ src/bin/day08.rs
@@ -0,0 +1,103 @@
+use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
+use std::vec::Vec;
+use ya_advent_lib::coords::Coord2D;
+use ya_advent_lib::grid::Grid;
+use ya_advent_lib::read::read_input;
+
+#[derive(Copy, Clone)]
+enum Cell {
+    Empty,
+    Antenna(char),
+}
+
+impl From<char> for Cell {
+    fn from(c: char) -> Self {
+        match c {
+            'a'..='z' | 'A'..='Z' | '0'..='9' => Cell::Antenna(c),
+            _ => Cell::Empty,
+        }
+    }
+}
+
+fn bothparts(input: &[String]) -> (usize, usize) {
+    let grid: Grid<Cell> = Grid::from_input(input, Cell::Empty, 0);
+    let mut antinodes: HashSet<Coord2D> = HashSet::new();
+    let mut antinodes2: HashSet<Coord2D> = HashSet::new();
+    let mut by_freq: HashMap<char, Vec<Coord2D>> = HashMap::new();
+    grid.iter_with_coord()
+        .filter_map(|(cell, x, y)| match cell {
+            Cell::Antenna(c) => Some((c, x, y)),
+            _ => None
+        })
+        .for_each(|(c, x, y)| {
+            by_freq.entry(c)
+            .and_modify(|e| e.push((x, y).into()))
+            .or_insert(Vec::from_iter([(x, y).into()]));
+        });
+    by_freq.into_values()
+        .filter(|lst| lst.len() > 1)
+        .for_each(|lst| {
+            lst.iter().tuple_combinations()
+                .for_each(|(&a, &b)| {
+                    antinodes2.insert(a);
+                    antinodes2.insert(b);
+                    let diff = b - a;
+                    let mut n1 = a - diff;
+                    let mut first = true;
+                    while grid.contains_coord(n1) {
+                        if first {
+                            antinodes.insert(n1);
+                            first = false;
+                        }
+                        antinodes2.insert(n1);
+                        n1 -= diff;
+                    }
+                    let mut n2 = b + diff;
+                    first = true;
+                    while grid.contains_coord(n2) {
+                        if first {
+                            antinodes.insert(n2);
+                            first = false;
+                        }
+                        antinodes2.insert(n2);
+                        n2 += diff;
+                    }
+                });
+         });
+    (antinodes.len(), antinodes2.len())
+}
+
+fn main() {
+    let input: Vec<String> = read_input();
+    let (part1, part2) = bothparts(&input);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ya_advent_lib::read::test_input;
+
+    #[test]
+    fn day08_test() {
+        let input: Vec<String> = test_input(
+"............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+");
+        let (part1, part2) = bothparts(&input);
+        assert_eq!(part1, 14);
+        assert_eq!(part2, 34);
+    }
+}
